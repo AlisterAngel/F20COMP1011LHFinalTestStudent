e616258c05cb77aa4d399ddb5571a9bf1f75afc0
Riley Bosetti <55286231+AlisterAngel@users.noreply.github.com>
2020-12-10 13:25:10 -0500

    Customer class

--- src/Models/Customer.java
+++ src/Models/Customer.java
@@ -3,5 +3,55 @@ package Models;
 import java.util.ArrayList;
 
 public class Customer {
+    private Integer id;
+    private String firstName, lastName, phoneNumber;
+    private ArrayList<Product> purchased;
 
+    public Customer(Integer id, String firstName, String lastName, String phoneNumber, ArrayList<Product> purchased) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.purchased = purchased;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public ArrayList<Product> getPurchased() {
+        return purchased;
+    }
+
+    public void setPurchased(ArrayList<Product> purchased) {
+        this.purchased = purchased;
+    }
 }
